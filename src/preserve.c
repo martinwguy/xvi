martinwguy/xvi

commit 89cec539381cb7ebfcdc1f5e80b0b6ff0b5d2fe2

Drop "window" parameter from most functions
The window parameter to most functions was almost always curwin.
This patch arranged some code so that it's always curwin, and
removes the window parameter. Bottom-level functions just pick
it up from the global curwin. Compiled code saving: 4K.

src/preserve.c
@@ -42,16 +42,15 @@ char	*psv_strings[] =
 };
 
 /*
- * Open temporary file for given buffer.
+ * Open temporary file for current buffer.
  */
 static FILE *
-psvfile(window)
-Xviwin	*window;
+psvfile()
 {
     register Buffer	*buffer;
     FILE		*fp;
 
-    buffer = window->w_buffer;
+    buffer = curwin->w_buffer;
 
     if (buffer->b_tempfname == NULL) {
 	char	*fname;
@@ -61,38 +60,35 @@ Xviwin	*window;
 	    fname = "unnamed";
 	buffer->b_tempfname = tempfname(fname);
 	if (buffer->b_tempfname == NULL) {
-	    show_error(window, out_of_memory);
+	    show_error(out_of_memory);
 	    return(NULL);
 	}
     }
     fp = fopenwb(buffer->b_tempfname);
     if (fp == NULL) {
-	show_error(window, "Can't open preserve file %s",
-					     buffer->b_tempfname);
+	show_error(buffer->b_tempfname);
 	/*
 	 * This can happen asynchronously, so put
 	 * the cursor back in the right place.
 	 */
-	wind_goto(curwin);
-	VSflush(window->w_vs);
+	wind_goto();
+	VSflush(curwin->w_vs);
     }
     return(fp);
 }
 
 /*
- * Write contents of buffer to file & close file. Return TRUE if no
+ * Write contents of current buffer to file & close file. Return TRUE if no
  * errors detected.
  */
 static bool_t
-putbuf(wp, fp)
-Xviwin		*wp;
+putbuf(fp)
 register FILE	*fp;
 {
     unsigned long	l1, l2;
 
-    if (put_file(wp, fp, (Line *) NULL, (Line *) NULL, &l1, &l2) == FALSE) {
-	show_error(wp, "Error writing preserve file %s",
-					     wp->w_buffer->b_tempfname);
+    if (put_file(fp, (Line *) NULL, (Line *) NULL, &l1, &l2) == FALSE) {
+	show_error(curwin->w_buffer->b_tempfname);
 	return(FALSE);
     } else {
 	return(TRUE);
@@ -113,8 +109,7 @@ register FILE	*fp;
  * Return FALSE if an error occurs during preservation, otherwise TRUE.
  */
 bool_t
-preservebuf(window)
-Xviwin	*window;
+preservebuf()
 {
     FILE	*fp;
     Buffer	*bp;
@@ -128,7 +123,7 @@ Xviwin	*window;
 	    /*
 	     * If there is a preserve file already ...
 	     */
-	    (bp = window->w_buffer)->b_tempfname != NULL
+	    (bp = curwin->w_buffer)->b_tempfname != NULL
 	    &&
 	    exists(bp->b_tempfname)
 	    &&
@@ -144,12 +139,12 @@ Xviwin	*window;
 	return(TRUE);
     }
 
-    fp = psvfile(window);
+    fp = psvfile();
     if (fp == NULL) {
 	return(FALSE);
     }
 
-    return(putbuf(window, fp));
+    return(putbuf(fp));
 }
 
 /*
@@ -174,23 +169,23 @@ Buffer	*buffer;
 bool_t
 exPreserveAllBuffers()
 {
-    Xviwin		*wp;
+    Xviwin		*oldcurwin;
     bool_t		psvstatus = TRUE;
 
-    wp = curwin;
+    /* Cycle "curwin" through all the open windows, preserving each */
     do {
-	if (is_modified(wp->w_buffer)) {
+	if (is_modified(curwin->w_buffer)) {
 	    FILE	*fp;
 
-	    fp = psvfile(wp);
+	    fp = psvfile();
 	    if (fp != NULL) {
-		if (!putbuf(wp, fp))
+		if (!putbuf(fp))
 		    psvstatus = FALSE;
 	    } else {
 		psvstatus = FALSE;
 	    }
 	}
-    } while ((wp = xvNextWindow(wp)) != curwin);
+    } while ((curwin = xvNextWindow(curwin)) != oldcurwin);
 
     return(psvstatus);
 }

src/alloc.c
@@ -148,7 +148,7 @@ size_t size;
 
     if ((p = malloc(size)) == NULL) {
 	if (echo & e_ALLOCFAIL) {
-	    show_error(curwin, out_of_memory);
+	    show_error(out_of_memory);
 	}
     }
     return(p);
@@ -163,7 +163,7 @@ size_t size;
 
     if ((p = realloc(ref, size)) == NULL) {
 	if (echo & e_ALLOCFAIL) {
-	    show_error(curwin, out_of_memory);
+	    show_error(out_of_memory);
 	}
     }
     return(p);
@@ -181,14 +181,14 @@ size_t num, size;
     if ((size != 0) && ((total / size) != num)) {
 	/* overflow */
 	if (echo & e_ALLOCFAIL) {
-	    show_error(curwin, "Allocation size overflow!");
+	    show_error("Allocation size overflow!");
 	}
 	return(NULL);
     }
 
     if ((p = malloc(total)) == NULL) {
 	if (echo & e_ALLOCFAIL) {
-	    show_error(curwin, out_of_memory);
+	    show_error(out_of_memory);
 	}
     } else {
 	memset(p, 0, total);
@@ -196,7 +196,7 @@ size_t num, size;
 #else
     if ((p = calloc(num, size)) == NULL) {
 	if (echo & e_ALLOCFAIL) {
-	    show_error(curwin, out_of_memory);
+	    show_error(out_of_memory);
 	}
     }
 #endif

src/cmdline.c
@@ -510,19 +510,19 @@ bool_t	interactive;			/* true if reading from tty */
 	 * Otherwise, it's just a simple shell command.
 	 */
 	if (l_line != NULL) {
-	    specify_pipe_range(curwin, l_line, u_line);
-	    if (!do_pipe(curwin, arg)) {
+	    specify_pipe_range(l_line, u_line);
+	    if (!do_pipe(arg)) {
 		error++;
 	    }
 	} else {
-	    if (!exShellCommand(curwin, arg)) {
+	    if (!exShellCommand(arg)) {
 		error++;
 	    }
 	}
 	break;
 
     case EX_ARGS:
-	if (!exArgs(curwin)) {
+	if (!exArgs()) {
 	    error++;
 	}
 	break;
@@ -530,11 +530,11 @@ bool_t	interactive;			/* true if reading from tty */
     case EX_BUFFER:
 	if (arg != NULL)
 	    echo &= ~(e_SCROLL | e_REPORT | e_SHOWINFO);
-	if (!exNewBuffer(curwin, arg, 0)) {
+	if (!exNewBuffer(arg, 0)) {
 	    error++;
 	}
-	move_window_to_cursor(curwin);
-	redraw_window(curwin, FALSE);
+	move_window_to_cursor();
+	redraw_window(FALSE);
 	break;
 
     case EX_CHDIR:
@@ -549,7 +549,7 @@ bool_t	interactive;			/* true if reading from tty */
 
 	    if ((dirp = alloc(MAXPATHLEN + 2)) != NULL &&
 		getcwd(dirp, MAXPATHLEN + 2) != NULL) {
-		show_message(curwin, "%s", dirp);
+		show_message("%s", dirp);
 	    }
 	    if (dirp) {
 		free(dirp);
@@ -559,7 +559,7 @@ bool_t	interactive;			/* true if reading from tty */
     }
 
     case EX_CLOSE:
-	if (!exCloseWindow(curwin, exclam)) {
+	if (!exCloseWindow(exclam)) {
 	    error++;
 	}
 	break;
@@ -590,20 +590,20 @@ bool_t	interactive;			/* true if reading from tty */
 		break;
 	    }
 	}
-	update_sline(curwin);
+	update_sline();
 	break;
     }
 
     case EX_EDIT:
     case EX_VISUAL:			/* treat :vi as :edit */
 	echo &= ~(e_SCROLL | e_REPORT | e_SHOWINFO);
-	if (!exEditFile(curwin, exclam, arg)) {
+	if (!exEditFile(exclam, arg)) {
 	    error++;
 	}
-	move_window_to_cursor(curwin);
+	move_window_to_cursor();
 	xvUpdateAllBufferWindows(curbuf);
 #if 0
-	show_file_info(curwin, TRUE);
+	show_file_info(TRUE);
 #endif
 	break;
 
@@ -612,21 +612,21 @@ bool_t	interactive;			/* true if reading from tty */
 	break;
 
     case EX_FILE:
-	exShowFileStatus(curwin, arg);
+	exShowFileStatus(arg);
 	break;
 
     case EX_GLOBAL:
-	if (!exGlobal(curwin, l_line, u_line, arg, !exclam)) {
+	if (!exGlobal(l_line, u_line, arg, !exclam)) {
 	    error++;
 	}
 	break;
 
     case EX_HELP:
-	exHelp(curwin);
+	exHelp();
 	break;
 
     case EX_JOIN:
-	if (!exJoin(curwin, l_line, u_line, exclam)) {
+	if (!exJoin(l_line, u_line, exclam)) {
 	    error++;
 	}
 	break;
@@ -673,7 +673,7 @@ bool_t	interactive;			/* true if reading from tty */
 	 * xvUpdateAllBufferWindows() as required, so we don't
 	 * have to do any of that here.
 	 */
-	if (!exNext(curwin, argc, argv, exclam)) {
+	if (!exNext(argc, argv, exclam)) {
 	    error++;
 	}
 	break;
@@ -698,8 +698,8 @@ bool_t	interactive;			/* true if reading from tty */
 	    lastline = u_line->l_next;
 	}
 	do_line_numbers = (Pb(P_number) || command == EX_NUMBER);
-	disp_init(curwin, show_line, (int) curwin->w_ncols,
-				command == EX_LIST || Pb(P_list));
+	disp_init(show_line, (int) curwin->w_ncols,
+		  command == EX_LIST || Pb(P_list));
 	break;
 
     case EX_PUT:
@@ -713,33 +713,33 @@ bool_t	interactive;			/* true if reading from tty */
 	    where.p_index = curwin->w_cursor->p_index;
 	    where.p_line = curwin->w_cursor->p_line;
 	}
-	do_put(curwin, &where, FORWARD, '@');
+	do_put(&where, FORWARD, '@');
 	break;
     }
 
     case EX_QUIT:
-	exQuit(curwin, exclam);
+	exQuit(exclam);
 	break;
 
     case EX_REWIND:
-	if (!exRewind(curwin, exclam)) {
+	if (!exRewind(exclam)) {
 	    error++;
 	}
 	break;
 
     case EX_READ:
-	if (!exReadFile(curwin, arg, (l_line != NULL) ?
+	if (!exReadFile(arg, (l_line != NULL) ?
 				l_line : curwin->w_cursor->p_line)) {
 	    error++;
 	}
 	break;
 
     case EX_SET:
-	exSet(curwin, argc, argv, interactive);
+	exSet(argc, argv, interactive);
 	break;
 
     case EX_SHELL:
-	exInteractiveShell(curwin, exclam);
+	exInteractiveShell(exclam);
 	break;
 
     case EX_SOURCE:
@@ -747,7 +747,7 @@ bool_t	interactive;			/* true if reading from tty */
 	    badcmd(interactive, "Missing filename");
 	} else if (exSource(interactive, arg) && interactive) {
 #if 0
-	    show_file_info(curwin, TRUE);
+	    show_file_info(TRUE);
 #endif
 	    ;
 	} else {
@@ -759,7 +759,7 @@ bool_t	interactive;			/* true if reading from tty */
 	/*
 	 * "split".
 	 */
-	exSplitWindow(curwin);
+	exSplitWindow();
 	break;
 
     case EX_SUBSTITUTE:
@@ -767,7 +767,7 @@ bool_t	interactive;			/* true if reading from tty */
     case EX_TILDE:
     {
 	long		nsubs;
-	register long	(*func) P((Xviwin *, Line *, Line *, char *));
+	register long	(*func) P((Line *, Line *, char *));
 
 	switch (command) {
 	case EX_SUBSTITUTE:
@@ -780,15 +780,15 @@ bool_t	interactive;			/* true if reading from tty */
 	    func = exTilde;
 	}
 
-	nsubs = (*func)(curwin, l_line, u_line, arg);
+	nsubs = (*func)(l_line, u_line, arg);
 	if (nsubs == 0) {
 	    error++;
 	}
 	xvUpdateAllBufferWindows(curbuf);
-	cursupdate(curwin);
-	begin_line(curwin, TRUE);
+	cursupdate();
+	begin_line(TRUE);
 	if (nsubs >= Pn(P_report)) {
-	    show_message(curwin, "%ld substitution%c",
+	    show_message("%ld substitution%c",
 			nsubs,
 			(nsubs > 1) ? 's' : ' ');
 	}
@@ -796,27 +796,27 @@ bool_t	interactive;			/* true if reading from tty */
     }
 
     case EX_SUSPEND:
-	exSuspend(curwin, exclam);
+	exSuspend(exclam);
 	break;
 
     case EX_TAG:
-	if (!exTag(curwin, arg, exclam, TRUE, TRUE)) {
+	if (!exTag(arg, exclam, TRUE, TRUE)) {
 	    error++;
 	}
 	break;
 
     case EX_UNDO:
-	undo(curwin);
+	undo();
 	break;
 
     case EX_V:
-	if (!exGlobal(curwin, l_line, u_line, arg, FALSE)) {
+	if (!exGlobal(l_line, u_line, arg, FALSE)) {
 	    error++;;
 	}
 	break;
 
     case EX_VERSION:
-	show_message(curwin, Version);
+	show_message(Version);
 	break;
 
     case EX_WN:
@@ -824,12 +824,11 @@ bool_t	interactive;			/* true if reading from tty */
 	 * This is not a standard "vi" command, but it
 	 * is provided in PC vi, and it's quite useful.
 	 */
-	if (exWriteToFile(curwin, (char *) NULL, (Line *) NULL, (Line *) NULL,
-								exclam)) {
+	if (exWriteToFile((char *)NULL, (Line *)NULL, (Line *)NULL, exclam)) {
 	    /*
 	     * See comment for EX_NEXT (above).
 	     */
-	    if (!exNext(curwin, 0, argv, exclam)) {
+	    if (!exNext(0, argv, exclam)) {
 		error++;
 	    }
 	} else {
@@ -838,7 +837,7 @@ bool_t	interactive;			/* true if reading from tty */
 	break;
 
     case EX_WQ:
-	exWQ(curwin, arg, exclam);
+	exWQ(arg, exclam);
 	if (State != EXITING) error++;
 	break;
 
@@ -851,22 +850,22 @@ bool_t	interactive;			/* true if reading from tty */
 		    arg = NULL;
 		}
 	    } else {
-		show_error(curwin, "Write forms are 'w' and 'w>>'");
+		show_error("Write forms are 'w' and 'w>>'");
 		error++;
 		break;
 	    }
-	    if (!exAppendToFile(curwin, arg, l_line, u_line, exclam)) {
+	    if (!exAppendToFile(arg, l_line, u_line, exclam)) {
 		error++;
 	    }
 	} else {
-	    if (!exWriteToFile(curwin, arg, l_line, u_line, exclam)) {
+	    if (!exWriteToFile(arg, l_line, u_line, exclam)) {
 		error++;
 	    }
 	}
 	break;
 
     case EX_XIT:
-	if (!exXit(curwin)) {
+	if (!exXit()) {
 	    error++;
 	}
 	break;
@@ -878,11 +877,11 @@ bool_t	interactive;			/* true if reading from tty */
 	break;
 
     case EX_EXBUFFER:
-	yp_stuff_input(curwin, arg[0], FALSE);
+	yp_stuff_input(arg[0], FALSE);
 	break;
 
     case EX_EQUALS:
-	exEquals(curwin, l_line);
+	exEquals(l_line);
 	break;
 
     case EX_LSHIFT:
@@ -894,7 +893,7 @@ bool_t	interactive;			/* true if reading from tty */
 	    u_line = l_line;
 	}
 	tabinout((command == EX_LSHIFT) ? '<' : '>', l_line, u_line);
-	begin_line(curwin, TRUE);
+	begin_line(TRUE);
 	xvUpdateAllBufferWindows(curbuf);
 	break;
 
@@ -909,8 +908,8 @@ bool_t	interactive;			/* true if reading from tty */
 	    if (l_line == curbuf->b_line0) {
 		l_line = l_line->l_next;
 	    }
-	    move_cursor(curwin, l_line, 0);
-	    begin_line(curwin, TRUE);
+	    move_cursor(l_line, 0);
+	    begin_line(TRUE);
 	}
 	break;
 
@@ -1304,8 +1303,7 @@ get_line(cpp, startline, lpp)
 
     case '/':
     case '?':
-	pos = linesearch(curwin, startline,
-			(c == '/') ? FORWARD : BACKWARD, &cp);
+	pos = linesearch(startline, (c == '/') ? FORWARD : BACKWARD, &cp);
 	if (pos == NULL) {
 	    return FALSE;
 	}
@@ -1331,7 +1329,7 @@ get_line(cpp, startline, lpp)
     {
 	pos = getmark(*cp++, curbuf);
 	if (pos == NULL) {
-	    show_error(curwin, "Unknown mark");
+	    show_error("Unknown mark");
 	    return FALSE;
 	}
 	break;
@@ -1401,7 +1399,7 @@ bool_t	interactive;
 char	*str;
 {
     if (interactive) {
-	show_error(curwin, str);
+	show_error(str);
     }
     /* Halt any macros being executed */
     unstuff();
@@ -1449,14 +1447,14 @@ char	*str;
 	    switch (*from) {
 	    case '%':
 		if (curbuf->b_filename == NULL) {
-		    show_error(curwin, "Current filename is not set");
+		    show_error("Current filename is not set");
 		    return(NULL);
 		}
 		(void) lformat(&newstr, "%s", curbuf->b_filename);
 		break;
 	    case '#':
 	        if (alt_file_name() == NULL) {
-		    show_error(curwin, "Alternate filename is not set");
+		    show_error("Alternate filename is not set");
 		    return(NULL);
 		}
 		(void) lformat(&newstr, "%s", alt_file_name());

src/cmdmode.c
@@ -33,29 +33,28 @@ static	unsigned int	inend = 0;		/* one past the last char */
 static	unsigned char	colposn[CMDSZ];		/* holds n chars per char */
 
 /*
- * cmd_init(window, firstch)
+ * cmd_init(firstch)
  *
  * Initialise command line input.
  */
 void
-cmd_init(win, firstch)
-Xviwin	*win;
+cmd_init(firstch)
 int	firstch;
 {
     if (inpos > 0) {
-	show_error(win, "Internal error: re-entered command line input mode");
+	show_error("Internal error: re-entered command line input mode");
 	return;
     }
 
     State = CMDLINE;
 
-    flexclear(&win->w_statusline);
-    (void) flexaddch(&win->w_statusline, firstch);
+    flexclear(&curwin->w_statusline);
+    (void) flexaddch(&curwin->w_statusline, firstch);
     inbuf[0] = firstch;
     colposn[0] = 0;
     inpos = 1; inend = 1;
     colposn[1] = 1;
-    update_cline(win, colposn[1]);
+    update_cline(colposn[1]);
 }
 
 /*
@@ -95,7 +94,7 @@ common_prefix(s)
 }
 
 /*
- * cmd_input(window, character)
+ * cmd_input(character)
  *
  * Deal with command line input. Takes an input character and returns
  * one of cmd_CANCEL (meaning they deleted past the prompt character),
@@ -104,11 +103,10 @@ common_prefix(s)
  * mode to be in).
  *
  * Once cmd_COMPLETE has been returned, it is possible to call
- * get_cmd(win) to obtain the command line.
+ * get_cmd() to obtain the command line.
  */
 Cmd_State
-cmd_input(win, ch)
-Xviwin	*win;
+cmd_input(ch)
 int	ch;
 {
     unsigned		len;
@@ -122,7 +120,7 @@ int	ch;
 
     /* Nuls are disallowed whether literal or not */
     if (ch == CTRL('@')) {
-	beep(win);
+	beep();
 	return(cmd_INCOMPLETE);
     }
 
@@ -143,7 +141,7 @@ int	ch;
 	    inbuf[inend] = '\0';	/* terminate input line */
 	    inpos = 0; inend = 0;
 	    State = NORMAL;		/* return state to normal */
-	    update_sline(win);		/* line is now a message line */
+	    update_sline();	/* line is now a message line */
 	    return(cmd_COMPLETE);	/* and indicate we are done */
 
 	case '\b':		/* backspace or delete */
@@ -181,7 +179,7 @@ int	ch;
 	      /* Update the screen columns */
 	      for (i=inpos; i <= inend; i++) colposn[i] -= len;
 	      /* Move the end of the status line down to fill the gap */
-              stat = &win->w_statusline.fxb_chars[win->w_statusline.fxb_rcnt];
+              stat = &curwin->w_statusline.fxb_chars[curwin->w_statusline.fxb_rcnt];
 	      memmove(stat+colposn[inpos], stat+colposn[inpos]+len,
 		      colposn[inend]-colposn[inpos]);
 	    }
@@ -194,9 +192,9 @@ int	ch;
 		return(cmd_CANCEL);
 	    }
 	    len = colposn[inend];
-	    while (flexlen(&win->w_statusline) > len)
-		flexrmchar(&win->w_statusline);
-	    update_cline(win,colposn[inpos]);
+	    while (flexlen(&curwin->w_statusline) > len)
+		flexrmchar(&curwin->w_statusline);
+	    update_cline(colposn[inpos]);
 	    return(cmd_INCOMPLETE);
 
 	case '\t':
@@ -216,7 +214,7 @@ int	ch;
 	    inbuf[inend] = '\0';	/* ensure word is terminated */
 	    to_expand = strrchr(inbuf, ' ');
 	    if (to_expand == NULL || *(to_expand + 1) == '\0') {
-		beep(win);
+		beep();
 		return(cmd_INCOMPLETE);
 	    } else {
 		to_expand++;
@@ -236,13 +234,13 @@ int	ch;
 		 */
 		inend = inpos = to_expand - inbuf - 1;
 		len = colposn[inpos - 1] + 1;
-		while (flexlen(&win->w_statusline) > len)
-		    flexrmchar(&win->w_statusline);
+		while (flexlen(&curwin->w_statusline) > len)
+		    flexrmchar(&curwin->w_statusline);
 		if (common_prefix(expansion) > 1)
-		    beep(win);
+		    beep();
 		stuff(" %s", expansion);
 	    } else {
-		beep(win);
+		beep();
 	    }
 
 	    return(cmd_INCOMPLETE);
@@ -251,16 +249,16 @@ int	ch;
 	case EOF:
 	case CTRL('U'):		/* line kill */
 	    inpos = 1; inend = 1;
-	    flexclear(&win->w_statusline);
-	    (void) flexaddch(&win->w_statusline, inbuf[0]);
-	    update_cline(win, colposn[inpos]);
+	    flexclear(&curwin->w_statusline);
+	    (void) flexaddch(&curwin->w_statusline, inbuf[0]);
+	    update_cline(colposn[inpos]);
 	    return(cmd_INCOMPLETE);
 
 	case CTRL('C'):
 	case ESC:
 	    inpos = 0; inend = 0;
-	    flexclear(&win->w_statusline);
-	    update_cline(win, colposn[inpos]);
+	    flexclear(&curwin->w_statusline);
+	    update_cline(colposn[inpos]);
 	    State = NORMAL;
 	    return(cmd_CANCEL);
 
@@ -269,27 +267,27 @@ int	ch;
 	case K_LARROW:
 	    if (inpos > 1) {
 		--inpos;
-	        update_cline(win, colposn[inpos]);
+	        update_cline(colposn[inpos]);
 	    }
-	    else beep(win);
+	    else beep();
 	    return(cmd_INCOMPLETE);
 
 	case K_RARROW:
 	    if (inpos < inend) {
 		++inpos;
-	        update_cline(win, colposn[inpos]);
+	        update_cline(colposn[inpos]);
 	    }
-	    else beep(win);
+	    else beep();
 	    return(cmd_INCOMPLETE);
 
 	case K_HOME:
 	    inpos = 1;
-	    update_cline(win, colposn[inpos]);
+	    update_cline(colposn[inpos]);
 	    return(cmd_INCOMPLETE);
 
 	case K_END:
 	    inpos = inend;
-	    update_cline(win, colposn[inpos]);
+	    update_cline(colposn[inpos]);
 	    return(cmd_INCOMPLETE);
 
 	/*
@@ -308,7 +306,7 @@ int	ch;
 	case K_CGRAVE:
 	case K_PGDOWN:
 	case K_PGUP:
-	    beep(win);
+	    beep();
 	    return(cmd_INCOMPLETE);
 
 	default:
@@ -336,11 +334,11 @@ int	ch;
 	}
 #endif
 	/* Move the rest of the status line down */
-	stat = &win->w_statusline.fxb_chars[win->w_statusline.fxb_rcnt];
+	stat = &curwin->w_statusline.fxb_chars[curwin->w_statusline.fxb_rcnt];
 	memmove(stat+colposn[inpos], stat+colposn[inpos]+1,
 		colposn[inend]-colposn[inpos]);
 	/* and remove its last character */
-	flexrmchar(&win->w_statusline);
+	flexrmchar(&curwin->w_statusline);
 
 	literal_next = FALSE;
     }
@@ -356,8 +354,8 @@ int	ch;
 	endposn = colposn[inend];
 	w = vischar(ch, &p, -1);
 	if (inend >= sizeof(inbuf) - 1
-	    || endposn + w > win->w_ncols - 1) {
-	    beep(win);
+	    || endposn + w > curwin->w_ncols - 1) {
+	    beep();
 	} else {
 	    int i;
 	    memmove(inbuf+inpos+1, inbuf+inpos, inend-inpos);
@@ -366,14 +364,14 @@ int	ch;
 	    inend++; inbuf[inpos++] = ch;
 	    colposn[inpos] = colposn[inpos-1] + w;
 
-	    (void) lformat(&win->w_statusline, "%s", p);
+	    (void) lformat(&curwin->w_statusline, "%s", p);
 	    /* That appended the representation of the char to the
 	     * status line, extending the flexbuf, but we were supposed
 	     * to insert the new char, not append it, so move the rest
 	     * of the status line up, then deposit the new char(s) in
 	     * the hole that this leaves.
 	     */
-            stat = &win->w_statusline.fxb_chars[win->w_statusline.fxb_rcnt];
+            stat = &curwin->w_statusline.fxb_chars[curwin->w_statusline.fxb_rcnt];
             memmove(stat+colposn[inpos],
 		    stat+colposn[inpos-1],
 		    colposn[inend-1]-colposn[inpos-1]+1);
@@ -383,7 +381,7 @@ int	ch;
 	     * If we just displayed the ^ for a literal next character,
 	     * the cursor should be shown on the ^.
 	     */
-	    update_cline(win, colposn[inpos] - literal_next);
+	    update_cline(colposn[inpos] - literal_next);
 	}
     }
 
@@ -392,8 +390,7 @@ int	ch;
 
 /*ARGSUSED*/
 char *
-get_cmd(win)
-Xviwin	*win;
+get_cmd()
 {
     return(inbuf);
 }

src/cursor.c
@@ -23,7 +23,7 @@
 
 #include "xvi.h"
 
-static	void	calc_position_in_line P((Xviwin *));
+static	void	calc_position_in_line P((void));
 
 /*
  * Update the window's variables which say where the cursor is.
@@ -33,25 +33,24 @@ static	void	calc_position_in_line P((Xviwin *));
  * figure out whether the cursor line has changed size or not.
  */
 void
-cursupdate(win)
-Xviwin	*win;
+cursupdate()
 {
     /*
      * Calculate physical lines from logical lines.
      */
-    win->w_row = cntplines(win, win->w_topline, win->w_cursor->p_line);
-    win->w_c_line_size = plines(win, win->w_cursor->p_line);
+    curwin->w_row = cntplines(curwin->w_topline, curwin->w_cursor->p_line);
+    curwin->w_c_line_size = plines(curwin->w_cursor->p_line);
 
     /*
      * Calculate new position within the line.
      */
-    calc_position_in_line(win);
+    calc_position_in_line();
 }
 
 static void
-calc_position_in_line(win)
-Xviwin	*win;
+calc_position_in_line()
 {
+    Xviwin		*win = curwin;
     register int	i;
     register char	*cltp;
     register int	ccol;

src/dispmode.c
@@ -32,20 +32,19 @@ static bool_t	disp_listmode;
  * The function returns NULL when no more lines are available.
  */
 void
-disp_init(win, func, colwidth, listmode)
-Xviwin		*win;
+disp_init(func, colwidth, listmode)
 char		*(*func) P((void));
 int		colwidth;
 bool_t		listmode;
 {
     State = DISPLAY;
     disp_func = func;
-    if (colwidth > win->w_ncols)
-	colwidth = win->w_ncols;
+    if (colwidth > curwin->w_ncols)
+	colwidth = curwin->w_ncols;
     disp_colwidth = colwidth;
-    disp_maxcol = (win->w_ncols / colwidth) * colwidth;
+    disp_maxcol = (curwin->w_ncols / colwidth) * colwidth;
     disp_listmode = listmode;
-    (void) disp_screen(win, '\0');
+    (void) disp_screen('\0');
 }
 
 /*
@@ -57,8 +56,7 @@ bool_t		listmode;
  * before redisplaying our editing screen.
  */
 bool_t
-disp_screen(win, ch)
-Xviwin	*win;
+disp_screen(ch)
 int	ch;
 {
     int		row;	/* current screen row */
@@ -66,15 +64,15 @@ int	ch;
     static bool_t	finished = FALSE;
     VirtScr		*vs;
 
-    vs = win->w_vs;
+    vs = curwin->w_vs;
 
     if (finished || kbdintr || ch == CTRL('C')) {
 	/*
 	 * Ensure that the window on the current buffer is
 	 * in the right place; then update the whole window.
 	 */
-	move_window_to_cursor(win);
-	redraw_all(win, TRUE);
+	move_window_to_cursor();
+	redraw_all(TRUE);
 	State = NORMAL;
 	/*
 	 * If they typed a colon, take it as valid input.

commit 2691c2603be60d1639d348b5ab5256e7536d1a06

Fix to 89cec539, which caused infinite looping in preserve

src/preserve.c
@@ -170,6 +170,7 @@ exPreserveAllBuffers()
     bool_t		psvstatus = TRUE;
 
     /* Cycle "curwin" through all the open windows, preserving each */
+    savecurwin = curwin;
     do {
 	if (is_modified(curbuf)) {
 	    FILE	*fp;