b0a6bdbfb640eeae7290f287d0259cefc741200f
Martin Guy <martinwguy@gmail.com>
2017-05-14 17:06:11 +0200

    Release 2.50.2

--- src/version.c
+++ src/version.c
@@ -20,7 +20,7 @@
 ***/
 
 #ifdef __DATE__
-    char	Version[] = "Xvi 2.50.1 " __DATE__;
+    char	Version[] = "Xvi 2.50.2 " __DATE__;
 #else
-    char	Version[] = "Xvi 2.50.1 4th May 2017";
+    char	Version[] = "Xvi 2.50.2 14th May 2017";
 #endif
