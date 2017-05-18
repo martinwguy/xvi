martinwguy/xvi

commit ff80b4fec3b5d6085cd04ce4af233365548e4d7e

Fix map-stuffed-text
Chars stuffed as part of a macro expansion were being added at the end
of the non-empty canonical queue, whereas they should be stuffed at the
beginning to maintain the input in the correct order.

This also adds some tests to newline-in-stuff to ensure that multiple
lines between the first and last text fragments are inserted in the
correct order.

src/yankput.c
@@ -714,6 +714,10 @@ oom:
  * The "vi_mode" parameter will be FALSE if the buffer should
  * be preceded by a ':' and followed by a '\n', i.e. it is the
  * result of a :@ command rather than a vi-mode @ command.
+ *
+ * POSIX: "After each line of a line-mode buffer,
+ * and all but the last line of a character mode buffer,
+ * behave as if a <newline> were entered as standard input."
  */
 void
 yp_stuff_input(name, vi_mode)
@@ -721,40 +725,73 @@ int	name;
 bool_t	vi_mode;
 {
     Yankbuffer	*yp_buf;
+    Line	*lp;
 
     yp_buf = yp_get_buffer(name);
     if (yp_buf == NULL) {
 	return;
     }
 
-    /*
-     * POSIX: "After each line of a line-mode buffer,
-     * and all but the last line of a character mode buffer,
-     * behave as if a <newline> were entered as standard input."
-     */
-    switch (yp_buf->y_type) {
-    case y_chars:
-	put(yp_buf->y_1st_text, vi_mode, yp_buf->y_2nd_text != NULL);
-	break;
+    if (!vi_mode) {
+	switch (yp_buf->y_type) {
+	case y_chars:
+	    put(yp_buf->y_1st_text, vi_mode, yp_buf->y_2nd_text != NULL);
+	    break;
 
-    case y_lines:
-	break;
+	case y_lines:
+	    break;
 
-    default:
-	show_error("Nothing to put!");
-	return;
-    }
-
-    if (yp_buf->y_line_buf != NULL) {
-	Line	*lp;
+	default:
+	    show_error("Nothing to put!");
+	    return;
+	}
 
 	for (lp = yp_buf->y_line_buf; lp != NULL; lp = lp->l_next) {
 	    put(lp->l_text, vi_mode, TRUE);
 	}
-    }
 
-    if (yp_buf->y_type == y_chars && yp_buf->y_2nd_text != NULL) {
-	put(yp_buf->y_2nd_text, vi_mode, FALSE);
+	if (yp_buf->y_type == y_chars && yp_buf->y_2nd_text != NULL) {
+	    put(yp_buf->y_2nd_text, vi_mode, FALSE);
+	}
+    } else {
+	/*
+	 * Characters from vi-mode @f commands are stuffed into the start of
+	 * the canonical queue so here we have to put() the elements in
+	 * reverse order for them to end up in the correct order.
+	 */
+	switch (yp_buf->y_type) {
+	case y_chars:
+	    if (yp_buf->y_2nd_text != NULL) {
+		put(yp_buf->y_2nd_text, vi_mode, FALSE);
+	    }
+	    break;
+
+	case y_lines:
+	    break;
+
+	default:
+	    show_error("Nothing to put!");
+	    return;
+	}
+
+	/* Insert the intervening lines in reverse order */
+	if (yp_buf->y_line_buf != NULL) {
+	    /* Go to the last element in the list of lines */
+	    for (lp = yp_buf->y_line_buf; lp->l_next != NULL; lp = lp->l_next)
+		;
+	    /*
+	     * Now work back towards the first one, including the first line.
+	     * The (lp = lp->l_prev) != NULL is always true, used to make it
+	     * go back to the first line inclusive.
+	     */
+	    do {
+		put(lp->l_text, vi_mode, TRUE);
+	    } while (lp != yp_buf->y_line_buf && (lp = lp->l_prev));
+	}
+
+	if (yp_buf->y_type == y_chars && yp_buf->y_1st_text != NULL) {
+	    put(yp_buf->y_1st_text, vi_mode, yp_buf->y_2nd_text != NULL);
+	}
     }
 }
 
@@ -764,10 +801,21 @@ char	*str;
 bool_t	vi_mode;
 bool_t	newline;
 {
-    stuff("%s%s%s",
-	    (!vi_mode && str[0] != ':') ? ":" : "",
-	    str,
-	    (!vi_mode || newline) ? "\n" : "");
+    if (vi_mode) {
+	/* stuff_to_map() inserts chars at the start of canon_queue so
+	 * insert a trailing newline, if any, before the text so that it
+	 * ends up after the text that it should follow.
+	 */
+	if (newline) {
+	    stuff_to_map("\n");
+	}
+	stuff_to_map(str);
+    } else {
+	stuff("%s%s%s",
+		str[0] != ':' ? ":" : "",
+		str,
+		newline ? "\n" : "");
+    }
 }
 
 /*

issues/open/map-stuffed-text
@@ -1,164 +1,0 @@
-Text stuffed with @ should go through the input map
-
-One of the reasons the maze test doesn't run is that xvi doesn't put
-text stuffed from a yank/named buffer into the input stream (e.g. @f).
-
-When we do this with the patch below, hanoi stops working, halting at:
-
-0  <-- cursor is at (2,0) on this 0
-07
-0654321
-
-
-T0123456n
-T0123456$
-/
-~
-We've tried unrolling the first loop iteration of hanoi and that doesn't
-help because it's halting near the end.
-
-We could unroll the whole hanoi test, creating a test piece by running
-hanoi with a working xvi and spitting out the input chars since it last
-changed and the new screen automatically, but it's a lot of effort!
-
-The xvi change is that test inserted with @ goes through the map table.
-
-Tested adding a parameter to turn mapping of stuffed text on and off and
-debug-printfing the stuffed text at the start of stuff_to_map() reveals that
-no mapped character are being stuffed but without the patch map() is called:
-WORKING:
-  PUT! [/6]
-  PUT! [/0]
-! PUT! [$]
-  PUT! [/5]
-  PUT! [/0]
-! PUT! [$]
-  PUT! [/5]
-  PUT! [/6]
-! PUT! [n]
-  PUT! [/4]
-+ PUT! [/0]
-+ PUT! [$]
-+ PUT! [/3]
-+ PUT! [/6]
-+ PUT! [n]
-  PUT! [/5]
-! PUT! [/4]
-! PUT! [$]
-but using stuff_to_map() for @-stuffed chars, map() is called with:
-BROKEN:
-  PUT! [/6]
-  PUT! [/0]
-! PUT! [/]
-  PUT! [/5]
-  PUT! [/0]
-! PUT! [/]
-  PUT! [/5]
-  PUT! [/6]
-! PUT! [/]
-  PUT! [/4]
-  PUT! [/5]
-! PUT! [/]
-  PUT! [/3]
-  PUT! [/0]
-! PUT! [/]
-  PUT! [/3]
-  PUT! [/4]
-! PUT! [/]
-
-Now, the three @-stuffs are @f @h @f in the L macro so it looks like @f
-is getting (or regurgitating) the wrong contents. @f is set by macro A "fyl
-four characters earlier in macro L.
-
-Experiment:
-To find out whether it's the storing or regurgitating/mapping, add printfs
-when named buffers are yanked to. If it's getting / it something to do with
-the yanking code. If it's getting $, n it's something to do with the putting
-code.
-
-Result:
-WORKING (no mapping of @stuffs):
-Yank to buffer @ C[]
-Yank to buffer @
-Yank to buffer @ C[89Z]
-Yank to buffer @
-Yank to buffer @
-Yank to buffer @ C[7]
-Yank to buffer f C[6]
-Yank to buffer h C[0]
-Yank to buffer f C[/6]
-Yank to buffer @ C[6]
-Yank to buffer h C[/0]
-Yank to buffer @ C[0]
-PUT! [/6]
-PUT! [/0]
-Yank to buffer f C[$]
-PUT! [$]
-BROKEN (mapping @stuffs):
-Yank to buffer @ C[]
-Yank to buffer @
-Yank to buffer @ C[89Z]
-Yank to buffer @
-Yank to buffer @
-Yank to buffer @ C[7]
-Yank to buffer f C[6]
-Yank to buffer h C[0]
-Yank to buffer f C[/6]
-Yank to buffer @ C[6]
-Yank to buffer h C[/0]
-Yank to buffer @ C[0]
-PUT! [/6]
-PUT! [/0]
-Yank to buffer f C[/]
-PUT! [/]
-HOWEVER if you run the broken one step by step, it gets the right result
-(yanks and puts $ into "f, not /). It only gets it wrong when you run the
-whole macro.
-This may be because stuffing into the canonical queue changes the order
-of teh input chacraters. For example, if saying L pushes 60 chars onto the
-canonical queue, and while processing that we push something else onto
-the canon queue, it'll come after the 60 chars, whereas if stuffed onto
-the mapped queue, it'll come before any of the 60 chars left in the canon.
-
-So try stuffing @-mapped chars onto the start of the canon queue, not the end.
-
-
-diff --git a/src/map.c b/src/map.c
-index a7c601e..9ac53a0 100644
---- a/src/map.c
-+++ b/src/map.c
-@@ -130,7 +130,7 @@ void
- stuff_to_map(s)
- char *s;
- {
--    lformat(&canon_queue, s);
-+    lformat(&canon_queue, "%s", s);
- }
-
- /* Undo all previous "stuff"'s, leaving the input buffer empty.
-diff --git a/src/yankput.c b/src/yankput.c
-index c2680d8..a91d5c0 100644
---- a/src/yankput.c
-+++ b/src/yankput.c
-@@ -759,10 +764,17 @@ char	*str;
- bool_t	vi_mode;
- bool_t	newline;
- {
--    stuff("%s%s%s",
--	    (!vi_mode && str[0] != ':') ? ":" : "",
--	    str,
--	    (!vi_mode || newline) ? "\n" : "");
-+    if (vi_mode) {
-+	stuff_to_map(str);
-+	if (newline) {
-+	    stuff_to_map("\n");
-+	}
-+    } else {
-+	stuff("%s%s%s",
-+		str[0] != ':' ? ":" : "",
-+		str,
-+		newline ? "\n" : "");
-+    }
- }
-
- /*

src/map.c
@@ -130,7 +130,7 @@ void
 stuff_to_map(s)
 char *s;
 {
-    lformat(&canon_queue, s);
+    flexinsstr(npos.mp_src, 0, s);
 }
 
 /* Undo all previous "stuff"'s, leaving the input buffer empty.

test/tests/newline-in-stuff
@@ -15,20 +15,28 @@ exec tclsh "$0" ${1+"$@"}
 #
 # Here we test that they get the newlines on the right places.
 
+# This only tests stuffing in vi mode (with, e.g., @f), which goes through
+# the mapping tables. We need to check ex mode (e.g. the :@f command).
+
 source scripts/term
 start_vi
 
-# One line char-based
+# One line char-based check that 1st_text is inserted
 test 10 "aaabc\rdef\rghi[esc]"	3 2 [list "aabc" "def" "ghi" "~"]
 test 11 "1G\"ay\$G\$@a[esc]"	3 5 [list "aabc" "def" "ghiabc" "~"]
 
-# Two lines char-based
+# Two lines char-based check that 2nd_text is inserted
 test 20 "1GcGaabc\rdef\rghi[esc]" 3 2 [list "aabc" "def" "ghi" "~"]
 test 21 "1G\"by/f\rG\$@b[esc]"	4 1 [list "aabc" "def" "ghiabc" "de" "~"]
 
-# Three lines char-based
+# Three lines char-based checks that l_lines is inserted
 test 30 "1GcGaabc\rdef\rghi[esc]" 3 2 [list "aabc" "def" "ghi" "~"]
 test 31 "1G\"cy/i\rG\$@c[esc]"	5 1 [list "aabc" "def" "ghiabc" "def" "gh" "~"]
+
+# Five lines char-based checks that l_lines are inserted and in the right order
+test 32 "1GcGaabc\rdef\rghi\rjkl\rmno[esc]" 5 2 [list "aabc" "def" "ghi" "jkl" "mno" "~"]
+test 33 "1G\"dy/o\rG\$@d[esc]"	9 1 [list "aabc" "def" "ghi" "jkl" "mnoabc" \
+					  "def" "ghi" "jkl" "mn" "~"]
 
 # One line line-based
 test 40 "1GcGaabc\rdef\rghi[esc]" 3 2 [list "aabc" "def" "ghi" "~"]

commit fd588735478b0b8280bd9f37af793310c2b27270

Fix bug introduced by ff80b4fec braking redo of an insertion
"ix[esc]." stopped working because it's stuffed in vi mode but
the last inserted text in "< doesn't want to go through the maps.

I thought that all vi-mode stuffs would be mapped and all ex-mode not
but the redo of an insertion with the . command needs to stuff in vi
mode but doesn't go through the mapping tables.

This change separates the vi_mode flag into two: vi_mode and map_it
to handle all the different cases.

src/cmdline.c
@@ -879,7 +879,7 @@ char	*cmdline;			/* optional command string */
 	break;
 
     case EX_EXBUFFER:
-	yp_stuff_input(arg[0], FALSE);
+	yp_stuff_input(arg[0], FALSE, FALSE);
 	break;
 
     case EX_EQUALS:

src/edit.c
@@ -120,7 +120,7 @@ int	c;
 	if (!wait_buffer) {
 	    c = '<';
 	}
-	yp_stuff_input(c, TRUE);
+	yp_stuff_input(c, TRUE, FALSE);
 	wait_buffer = FALSE;
 	return(FALSE);
     }
@@ -138,7 +138,7 @@ int	c;
 	     */
 	    if (curpos->p_line == Insertloc.p_line &&
 		curpos->p_index == Insertloc.p_index) {
-		yp_stuff_input('<', TRUE);
+		yp_stuff_input('<', TRUE, FALSE);
 		stuff("%c", ESC);
 	    } else {
 		/*
@@ -640,7 +640,7 @@ int	c;
 	if (!wait_buffer) {
 	    c = '<';
 	}
-	yp_stuff_input(c, TRUE);
+	yp_stuff_input(c, TRUE, FALSE);
 	wait_buffer = FALSE;
 	return(FALSE);
     }

src/vi_cmds.c
@@ -199,7 +199,7 @@ Cmd	*cmd;
 	break;
 
     case '@':
-	yp_stuff_input(cmd->cmd_ch2, TRUE);
+	yp_stuff_input(cmd->cmd_ch2, TRUE, TRUE);
 	break;
 
     /*
@@ -230,7 +230,7 @@ Cmd	*cmd;
 	 */
 	stuff("%s", flexgetstr(&Redo.r_fb));
 	if (Redo.r_mode != r_normal) {
-	    yp_stuff_input('<', TRUE);
+	    yp_stuff_input('<', TRUE, FALSE);
 	    if (Redo.r_mode == r_insert) {
 		stuff("%c", ESC);
 	    }

src/xvi.h
@@ -1269,5 +1269,5 @@ extern	void	init_yankput P((void));
 extern	bool_t	do_yank P((Posn *, Posn *, bool_t, int));
 extern	bool_t	yank_str P((int, char *, bool_t));
 extern	void	do_put P((Posn *, int, int));
-extern	void	yp_stuff_input P((int, bool_t));
+extern	void	yp_stuff_input P((int, bool_t, bool_t));
 extern	void	yp_push_deleted P((void));

src/yankput.c
@@ -73,7 +73,7 @@ typedef struct yankbuffer {
 
 static	Yankbuffer	yb[NBUFS];
 
-static	void		put P((char *, bool_t, bool_t));
+static	void		put P((char *, bool_t, bool_t, bool_t));
 static	Yankbuffer	*yp_get_buffer P((int));
 static	int		bufno P((int));
 static	Line		*copy_lines P((Line *, Line *));
@@ -715,14 +715,18 @@ oom:
  * be preceded by a ':' and followed by a '\n', i.e. it is the
  * result of a :@ command rather than a vi-mode @ command.
  *
+ * The "map_it" parameter says whether we should put the characters
+ * from the buffer through the map and/or map! tables.
+ *
  * POSIX: "After each line of a line-mode buffer,
  * and all but the last line of a character mode buffer,
  * behave as if a <newline> were entered as standard input."
  */
 void
-yp_stuff_input(name, vi_mode)
+yp_stuff_input(name, vi_mode, map_it)
 int	name;
 bool_t	vi_mode;
+bool_t	map_it;
 {
     Yankbuffer	*yp_buf;
     Line	*lp;
@@ -735,7 +739,7 @@ bool_t	vi_mode;
     if (!vi_mode) {
 	switch (yp_buf->y_type) {
 	case y_chars:
-	    put(yp_buf->y_1st_text, vi_mode, TRUE);
+	    put(yp_buf->y_1st_text, vi_mode, TRUE, map_it);
 	    break;
 
 	case y_lines:
@@ -747,22 +751,22 @@ bool_t	vi_mode;
 	}
 
 	for (lp = yp_buf->y_line_buf; lp != NULL; lp = lp->l_next) {
-	    put(lp->l_text, vi_mode, TRUE);
+	    put(lp->l_text, vi_mode, TRUE, map_it);
 	}
 
 	if (yp_buf->y_type == y_chars && yp_buf->y_2nd_text != NULL) {
-	    put(yp_buf->y_2nd_text, vi_mode, TRUE);
+	    put(yp_buf->y_2nd_text, vi_mode, TRUE, map_it);
 	}
     } else {
 	/*
-	 * Characters from vi-mode @f commands are stuffed into the start of
+	 * Characters from @f commands are stuffed into the start of
 	 * the canonical queue so here we have to put() the elements in
 	 * reverse order for them to end up in the correct order.
 	 */
 	switch (yp_buf->y_type) {
 	case y_chars:
 	    if (yp_buf->y_2nd_text != NULL) {
-		put(yp_buf->y_2nd_text, vi_mode, FALSE);
+		put(yp_buf->y_2nd_text, vi_mode, FALSE, map_it);
 	    }
 	    break;
 
@@ -785,31 +789,46 @@ bool_t	vi_mode;
 	     * go back to the first line inclusive.
 	     */
 	    do {
-		put(lp->l_text, vi_mode, TRUE);
+		put(lp->l_text, vi_mode, TRUE, map_it);
 	    } while (lp != yp_buf->y_line_buf && (lp = lp->l_prev));
 	}
 
 	if (yp_buf->y_type == y_chars && yp_buf->y_1st_text != NULL) {
-	    put(yp_buf->y_1st_text, vi_mode, yp_buf->y_2nd_text != NULL);
+	    put(yp_buf->y_1st_text, vi_mode, yp_buf->y_2nd_text != NULL, map_it);
 	}
     }
 }
 
+/*
+ * Stuff a string into the input.
+ *
+ * If vi_mode is FALSE, we're stuffing ex commands (the :@f command) so need
+ * to add a leading : if it's missing.
+ *
+ * If map_it is TRUE, we are stuffing input that needs to go through the
+ * :map and :map! tables. If FALSE, it's already been there (the Redo insert).
+ */
 static void
-put(str, vi_mode, newline)
+put(str, vi_mode, newline, map_it)
 char	*str;
 bool_t	vi_mode;
 bool_t	newline;
+bool_t	map_it;
 {
     if (vi_mode) {
-	/* stuff_to_map() inserts chars at the start of canon_queue so
+	/*
+	 * stuff_to_map() inserts chars at the start of canon_queue so
 	 * insert a trailing newline, if any, before the text so that it
 	 * ends up after the text that it should follow.
 	 */
-	if (newline) {
-	    stuff_to_map("\n");
+	if (map_it) {
+	    if (newline) {
+		stuff_to_map("\n");
+	    }
+	    stuff_to_map(str);
+	} else {
+	    stuff(newline ? "%s\n" : "%s", str);
 	}
-	stuff_to_map(str);
     } else {
 	stuff("%s%s%s",
 		str[0] != ':' ? ":" : "",